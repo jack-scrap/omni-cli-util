c3ed6dd8115ae63116fbfad57b503e0bc5fcd7b0
Jack <jackhasakeyboard@gmail.com>
2023-03-24 22:52:31 -0600

    Updated name of directory containing dynamic libraries to mirror main project where directory contains both regular objects and shared objects, recipe to install now works

--- omni_gen_steam_depot.sh
+++ omni_gen_steam_depot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir -p lib
+mkdir -p build
 
-cp build/*.so lib
+cp build/*.so build
 
 make
 zip build.zip omni lib/*.so res lvl player omni.py catch_out.py lvl_spec.json stat.json InstallScript.vdf Makefile
