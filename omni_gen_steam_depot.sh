9fd1c9ebe5de13140196d3eaa6d08547a279b280
Jack <jackhasakeyboard@gmail.com>
2023-03-24 22:50:35 -0600

    Added main Python script to depot upon generating

--- omni_gen_steam_depot.sh
+++ omni_gen_steam_depot.sh
@@ -5,4 +5,4 @@ mkdir -p lib
 cp build/*.so lib
 
 make
-zip build.zip omni lib/*.so res lvl player lvl_spec.json stat.json InstallScript.vdf Makefile
+zip build.zip omni lib/*.so res lvl player omni.py catch_out.py lvl_spec.json stat.json InstallScript.vdf Makefile
