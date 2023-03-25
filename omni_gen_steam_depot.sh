c2c0f1391d876c2d66283d806ef95cb3e2ab1c63
Jack <jackhasakeyboard@gmail.com>
2023-03-25 15:43:47 -0600

    Removed arbitrary generation of directory that's identical to build directory

--- omni_gen_steam_depot.sh
+++ omni_gen_steam_depot.sh
@@ -2,9 +2,5 @@
 
 LIBDIR=build
 
-mkdir -p $LIBDIR
-
-cp build/*.so $LIBDIR
-
 make
 zip -r build.zip omni $LIBDIR/*.so res lvl player omni.py catch_out.py lvl_spec.json stat.json installscript.vdf Makefile
