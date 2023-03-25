f27a175ba2196da3939b098e718d0bd97a88cba3
Jack <jackhasakeyboard@gmail.com>
2023-03-24 21:07:04 -0600

    Created dedicated directory for shared libraries

--- omni_gen_steam_depot.sh
+++ omni_gen_steam_depot.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+mkdir -p lib;
+
+cp build/*.so lib;
+
 make;
-zip build.zip omni build/*.so res lvl player lvl_spec.json stat.json;
+zip build.zip omni lib/*.so res lvl player lvl_spec.json stat.json;
