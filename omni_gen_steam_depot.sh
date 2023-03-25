b7b7e78c6b8942bbbd10248d33d397fb60715a93
Jack <jackhasakeyboard@gmail.com>
2023-03-24 22:05:56 -0600

    Added Makefile to depot upon generating, Steam install script depending on Make recipe

--- omni_gen_steam_depot.sh
+++ omni_gen_steam_depot.sh
@@ -5,4 +5,4 @@ mkdir -p lib;
 cp build/*.so lib;
 
 make;
-zip build.zip omni lib/*.so res lvl player lvl_spec.json stat.json InstallScript.vdf;
+zip build.zip omni lib/*.so res lvl player lvl_spec.json stat.json InstallScript.vdf Makefile;
