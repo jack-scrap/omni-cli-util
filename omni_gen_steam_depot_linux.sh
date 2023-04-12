949ef2dacc7af7f9db2212c0d894e5b37b0f1b5c
Jack <jackhasakeyboard@gmail.com>
2023-04-12 14:13:31 -0600

    Added external runtime dependencies to archive in script generating Steam depot for Linux

--- omni_gen_steam_depot_linux.sh
+++ omni_gen_steam_depot_linux.sh
@@ -12,4 +12,4 @@ rm -f build_linux.zip
 
 git pull $1
 make
-zip -r build_linux.zip omni $LIBDIR/*.so res lvl player/cfg player/doc omni.py catch_out.py lvl_spec.json stat.json installscript.vdf
+zip -r build_linux.zip omni $LIBDIR/*.so res lvl player/cfg player/doc omni.py catch_out.py lvl_spec.json stat.json dep installscript.vdf
