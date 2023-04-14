6c6c661de027cd95ab78e8b4047475c48f16f134
Jack <jackhasakeyboard@gmail.com>
2023-04-14 13:02:01 -0600

    Explicitly operated on the master branch when pulling from the original repo in script generating Steam depot for Linux

--- omni_gen_steam_depot_linux.sh
+++ omni_gen_steam_depot_linux.sh
@@ -10,6 +10,7 @@ LIBDIR=build
 
 rm -f build_linux.zip
 
-git pull $1
+git checkout master
+git pull $1 -b master
 make
 zip -r build_linux.zip omni $LIBDIR/*.so res lvl player/cfg player/doc omni.py catch_out.py lvl_spec.json stat.json dep installscript.vdf
