171218d6c022e48735ab4818f0183cbdc83f45ea
Jack <jackhasakeyboard@gmail.com>
2023-04-28 17:12:23 -0600

    Logged into Steam account instead of general anonymous login when building Steam depot

--- omni_build_steam_depot_linux.sh
+++ omni_build_steam_depot_linux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
+if [ $# -ne 3 ]; then
 	echo "Error: Wrong number of arguments"
 
 	exit 2
 fi
 
-steamcmd +login anonymous +run_app_build $1/scripts/app_build_2329020.vdf +quit
+steamcmd +login $2 $3 +run_app_build $1/scripts/app_build_2329020.vdf +quit
