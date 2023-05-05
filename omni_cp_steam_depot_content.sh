788a85afc7d9ad0ac23790699e86c895f4c80ed4
jack-scrap <jackhasakeyboard@gmail.com>
2023-05-05 11:59:00 -0600

    Copied install script from other project, now kept up to datewith the latest version and undoes changes done by being signed

--- omni_cp_steam_depot_content.sh
+++ omni_cp_steam_depot_content.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
 	echo "Error: Wrong number of arguments"
 
 	exit 2
@@ -10,11 +10,11 @@ CONTENT_ROOT=content
 
 INSTALL_SCRIPT=installscript_linux.vdf 
 
-find content -type f ! -name $INSTALL_SCRIPT -delete
-
 cp $1/omni $CONTENT_ROOT
 mkdir -p $CONTENT_ROOT/lib
 cp $1/lib/*.so $CONTENT_ROOT/lib
 cp -r $1/dep $CONTENT_ROOT
 cp $1/stat.json $1/lvl_spec.json $CONTENT_ROOT
 cp $1/omni.py $1/catch_out.py $CONTENT_ROOT
+
+cp $2/$INSTALL_SCRIPT $CONTENT_ROOT
