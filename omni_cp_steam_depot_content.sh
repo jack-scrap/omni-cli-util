c3fd9ec4c25a1a746a04c9d8ab30a23e184614ab
Jack <jackhasakeyboard@gmail.com>
2023-05-04 19:36:57 -0600

    Updated copying of content for Steam depot to reflect being ran from the base directory

--- omni_cp_steam_depot_content.sh
+++ omni_cp_steam_depot_content.sh
@@ -6,9 +6,11 @@ if [ $# -ne 1 ]; then
 	exit 2
 fi
 
-cp $1/omni .
-mkdir -p lib
-cp $1/lib/*.so lib
-cp -r $1/dep .
-cp $1/stat.json $1/lvl_spec.json .
-cp $1/omni.py $1/catch_out.py .
+CONTENT_ROOT=content
+
+cp $1/omni $CONTENT_ROOT
+mkdir -p $CONTENT_ROOT/lib
+cp $1/lib/*.so $CONTENT_ROOT/lib
+cp -r $1/dep $CONTENT_ROOT
+cp $1/stat.json $1/lvl_spec.json $CONTENT_ROOT
+cp $1/omni.py $1/catch_out.py $CONTENT_ROOT
