6f0c2a060d78c463cffe0086a51836395d41a7b3
Jack <jackhasakeyboard@gmail.com>
2023-05-04 19:28:52 -0600

    Updated dedicated directory for dynamic libraries to reflect main project

---
+++ omni_cp_steam_depot_content.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+	echo "Error: Wrong number of arguments"
+
+	exit 2
+fi
+
+cp $1/omni .
+mkdir -p lib
+cp $1/lib/*.so .
+cp -r $1/dep .
+cp $1/stat.json $1/lvl_spec.json .
+cp $1/omni.py $1/catch_out.py .
