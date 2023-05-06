f0e4da1a83d434b0882b4c2fa86bd9ec8f354ebb
jack-scrap <jackhasakeyboard@gmail.com>
2023-05-05 21:18:51 -0600

    Properly generated player directory, copied static resources when copying contents of project producing build for Steam depot

--- omni_cp_steam_depot_content.sh
+++ omni_cp_steam_depot_content.sh
@@ -14,6 +14,11 @@ cp $1/omni $CONTENT_ROOT
 mkdir -p $CONTENT_ROOT/lib
 cp $1/lib/*.so $CONTENT_ROOT/lib
 cp -r $1/dep $CONTENT_ROOT
+mkdir -p $CONTENT_ROOT/player
+cp -r $1/player/doc $CONTENT_ROOT/player
+cp -r $1/player/cfg $CONTENT_ROOT/player
+mkdir -p $CONTENT_ROOT/player/script
+cp -r $1/res $1/lvl $CONTENT_ROOT
 cp $1/stat.json $1/lvl_spec.json $CONTENT_ROOT
 cp $1/omni.py $1/catch_out.py $CONTENT_ROOT
 
